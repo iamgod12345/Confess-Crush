166daef68831315ceb728859c23f88a6689cf673
iamgod12345 <106958524+iamgod12345@users.noreply.github.com>
2022-06-06 16:08:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi đến người tôi yêu",
+    introTitle: 'Lê Hà My',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
